ac72831fe95bf049ec61634c9c58126028ba5aa4
kikumatsuharuna <kikumatsuharuna@192.168.1.13>
2021-05-03 11:15:17 +0900

    Revert "問題として表示される文字列に答えと同じ文字列が表示されないように変更"
    This reverts commit 7478111d65a6733fbccd4010ed5679eee5eeedd1.

--- src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -39,83 +39,164 @@ import java.util.List;
  */
 final class StaticWordLibrary extends WordLibrary {
 
-	private static final String[] WORD_LIST = { 
-			"abstraction", "ambiguous", "arithmetic", "backslash", "bitmap",
-			"circumstance", "combination", "consequently", "consortium", "decrementing", "dependency", "disambiguate",
-			"dynamic", "encapsulation", "equivalent", "expression", "facilitate", "fragment", "hexadecimal",
-			"implementation", "indistinguishable", "inheritance", "internet", "java", "localization", "microprocessor",
-			"navigation", "optimization", "parameter", "patrick", "pickle", "polymorphic", "rigorously",
-			"simultaneously", "specification", "structure", "lexical", "likewise", "management", "manipulate",
-			"mathematics", "hotjava", "vertex", "unsigned", "traditional", "python"
-			};
+    private static final String[] WORD_LIST = {
+        "abstraction",
+        "ambiguous",
+        "arithmetic",
+        "backslash",
+        "bitmap",
+        "circumstance",
+        "combination",
+        "consequently",
+        "consortium",
+        "decrementing",
+        "dependency",
+        "disambiguate",
+        "dynamic",
+        "encapsulation",
+        "equivalent",
+        "expression",
+        "facilitate",
+        "fragment",
+        "hexadecimal",
+        "implementation",
+        "indistinguishable",
+        "inheritance",
+        "internet",
+        "java",
+        "localization",
+        "microprocessor",
+        "navigation",
+        "optimization",
+        "parameter",
+        "patrick",
+        "pickle",
+        "polymorphic",
+        "rigorously",
+        "simultaneously",
+        "specification",
+        "structure",
+        "lexical",
+        "likewise",
+        "management",
+        "manipulate",
+        "mathematics",
+        "hotjava",
+        "vertex",
+        "unsigned",
+        "traditional",
+        "python"};
 
-	final static WordLibrary DEFAULT = new StaticWordLibrary();
+    /*private static final String[] SCRAMBLED_WORD_LIST = {
+        "batsartcoin",
+        "maibuguos",
+        "ratimhteci",
+        "abkclssha",
+        "ibmtpa",
+        "iccrmutsnaec",
+        "ocbmnitaoni",
+        "ocsnqeeutnyl",
+        "ocsnroitmu",
+        "edrcmeneitgn",
+        "edepdnneyc",
+        "idasbmgiauet",
+        "ydanicm",
+        "neacsplutaoni",
+        "qeiuaveltn",
+        "xerpseisno",
+        "aficilatet",
+        "rfgaemtn",
+        "ehaxedicalm",
+        "milpmeneatitno",
+        "niidtsniugsiahleb",
+        "niehiratcen",
+        "nietnret",
+        "ajav",
+        "olacilazitno",
+        "imrcpoorecssro",
+        "anivagitno",
+        "poitimazitno",
+        "aparemert",
+        "aprtcki",
+        "ipkcel",
+        "opylomprich",
+        "irogorsuyl",
+        "isumtlnaoesuyl",
+        "psceficitaoni",
+        "tsurtcreu",
+        "elixalc",
+        "ilekiwse",
+        "amanegemtn",
+        "aminupalet",
+        "amhtmetacsi",
+        "ohjtvaa",
+        "evtrxe",
+        "nuisngde",
+        "rtadlioiatn",
+        "nythpo"
+    };*/
+    
+    final static WordLibrary DEFAULT = new StaticWordLibrary();
 
-	/**
-	 * Singleton class.
-	 */
-	private StaticWordLibrary() {
-	}
+    /**
+     * Singleton class.
+     */
+    private StaticWordLibrary() {
+    }
 
-	/**
-	 * Gets the word at a given index.
-	 * 
-	 * @param idx index of required word
-	 * @return word at that index in its natural form
-	 */
-	public String getWord(int idx) {
-		return WORD_LIST[idx];
-	}
+    /**
+     * Gets the word at a given index.
+     * @param idx index of required word
+     * @return word at that index in its natural form
+     */
+    public String getWord(int idx) {
+        return WORD_LIST[idx];
+    }
 
-	/**
-	 * Gets the word at a given index in its scrambled form.
-	 * 
-	 * @param idx index of required word
-	 * @return word at that index in its scrambled form
-	 */
-	public String getScrambledWord(int idx) {
-		String suffledword;
-		String word = getWord(idx);
-		while(true) {
-			suffledword = suffle(word);
-			if (! (suffledword.equals(word))) break;
-		}
-		return suffledword;
-	}
-
-	public String suffle(String input) {
-
-		List<Character> characters = new ArrayList<Character>();
-		for (char c : input.toCharArray()) {
-			characters.add(c);
-		}
-		StringBuilder output = new StringBuilder(input.length());
-		while (characters.size() != 0) {
-			int randPicker = (int) (Math.random() * characters.size());
-			output.append(characters.remove(randPicker));
-		}
+    /**
+     * Gets the word at a given index in its scrambled form.
+     * @param idx index of required word
+     * @return word at that index in its scrambled form
+     */
+    public String getScrambledWord(int idx) {
+    	
+    	return suffle(getWord(idx));
+    	
+        //return SCRAMBLED_WORD_LIST[idx];
+    }
+    
+    public String suffle(String input) {
+    	
+    	List<Character> characters = new ArrayList<Character>();
+        for(char c:input.toCharArray()){
+            characters.add(c);
+        }
+        StringBuilder output = new StringBuilder(input.length());
+        while(characters.size()!=0){
+            int randPicker = (int)(Math.random()*characters.size());
+            output.append(characters.remove(randPicker));
+        }
+    	
 		return output.toString();
+    	
+    }
 
-	}
+    /**
+     * Gets the number of words in the library.
+     * @return the total number of plain/scrambled word pairs in the library
+     */
+    public int getSize() {
+        return WORD_LIST.length;
+    }
 
-	/**
-	 * Gets the number of words in the library.
-	 * 
-	 * @return the total number of plain/scrambled word pairs in the library
-	 */
-	public int getSize() {
-		return WORD_LIST.length;
-	}
-
-	/**
-	 * Checks whether a user's guess for a word at the given index is correct.
-	 * 
-	 * @param idx       index of the word guessed
-	 * @param userGuess the user's guess for the actual word
-	 * @return true if the guess was correct; false otherwise
-	 */
-	public boolean isCorrect(int idx, String userGuess) {
-		return userGuess.equals(getWord(idx));
-	}
+    /**
+     * Checks whether a user's guess for a word at the given index is correct.
+     * @param idx index of the word guessed
+     * @param userGuess the user's guess for the actual word
+     * @return true if the guess was correct; false otherwise
+     */
+    public boolean isCorrect(int idx, String userGuess) {
+        return userGuess.equals(getWord(idx));
+    }
 
 }
